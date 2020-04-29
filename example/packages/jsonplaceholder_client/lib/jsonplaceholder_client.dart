07f7e6091810d6084a622e79f0d8b0c0f86147ed
Felix Angelov <felangelov@gmail.com>
2020-04-29 17:19:45 -0500

    sort exports

--- example/packages/jsonplaceholder_client/lib/jsonplaceholder_client.dart
+++ example/packages/jsonplaceholder_client/lib/jsonplaceholder_client.dart
@@ -1,5 +1,5 @@
 library jsonplaceholder_client;
 
 export 'package:fresh/fresh.dart' show AuthenticationStatus;
-export 'src/models/models.dart';
 export 'src/jsonplaceholder_client.dart';
+export 'src/models/models.dart';
