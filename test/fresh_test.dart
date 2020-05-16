1952f5c7b28c12b723120d13e60963d3c940365c
Felix Angelov <felangelov@gmail.com>
2020-05-15 19:54:06 -0500

    test: add fresh tests

--- test/fresh_test.dart
+++ test/fresh_test.dart
@@ -1,11 +1,27 @@
 import 'package:fresh/fresh.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
+
+class MockTokenStorage extends Mock implements TokenStorage {}
 
 void main() {
   group('Fresh', () {
+    TokenStorage tokenStorage;
+
+    setUp(() {
+      tokenStorage = MockTokenStorage();
+    });
+
     test('throws AssertionError when tokenStorage is null', () {
       expect(
         () => Fresh(tokenStorage: null, refreshToken: (_, __) async {}),
+        throwsA(isA<AssertionError>()),
+      );
+    });
+
+    test('throws AssertionError when refreshToken is null', () {
+      expect(
+        () => Fresh(tokenStorage: tokenStorage, refreshToken: null),
         throwsA(isA<AssertionError>()),
       );
     });
