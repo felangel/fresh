309c311b67b868616ebaa0350ae6e150e44a9a48
Felix Angelov <felangelov@gmail.com>
2020-05-22 01:53:07 -0500

    feat: fresh_graphql v0.1.0

---
+++ packages/fresh_graphql/CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.1.0
+
+Initial Release of the library
+
+- Includes `FreshLink` graphql link with built-in token refresh.
--- packages/fresh_graphql/example/main.dart
+++ packages/fresh_graphql/example/main.dart
@@ -1,13 +1,13 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:fresh_graphql/fresh_graphql.dart';
 import 'package:graphql/client.dart';
 
 const getJobsQuery = r'''
   query GetJobs() {
     jobs {
-      id,
-      title,
-      locationNames,
-      isFeatured
+      title
     }
   }
 ''';
@@ -17,14 +17,23 @@ void main() async {
     tokenStorage: InMemoryTokenStorage(),
     refreshToken: (token, client) async {
       // Perform refresh and return new token
-      return token;
+      print('refreshing token!');
+      await Future.delayed(const Duration(seconds: 1));
+      if (Random().nextInt(3) == 0) {
+        throw RevokeTokenException();
+      }
+      return OAuth2Token(accessToken: 't0ps3cret_r3fresh3d!');
     },
-  );
+    shouldRefresh: (_) => Random().nextInt(1) == 0,
+    onRefreshFailure: () => print('refresh failed!'),
+  )..setToken(OAuth2Token(accessToken: 't0ps3cret!'));
   final graphQLClient = GraphQLClient(
     cache: InMemoryCache(),
     link: Link.from([freshLink, HttpLink(uri: 'https://api.graphql.jobs')]),
   );
-  await graphQLClient.query(
+  final result = await graphQLClient.query(
     QueryOptions(documentNode: gql(getJobsQuery)),
   );
+  print(result.data);
+  exit(0);
 }
